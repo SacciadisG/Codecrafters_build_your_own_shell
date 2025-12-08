0597864d4632fa5fe62fed834657b03b307f24de
Greg Sacciadis <greg.sacciadis@gmail.com>
2025-12-07 21:49:10 -0500

    Added buffer to read stdin & output stdout for invalid commands

--- app/main.go
+++ app/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -10,6 +12,23 @@ var _ = fmt.Fprint
 var _ = os.Stdout
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
+
 	fmt.Fprint(os.Stdout, "$ ")
+
+	buffer := make([]byte, 1024)
+	numBytesRead, err := os.Stdin.Read(buffer)
+
+	if !(err == nil || err == io.EOF) {
+		fmt.Fprintf(os.Stderr, "Error reading from stdin: %v\n", err)
+		return
+	}
+
+	// Switch on the command string (trimming the newline & return characters)
+	// For now, all commands are considered as 'invalid'
+	bufferedString := string(buffer[:numBytesRead])
+	commandString := strings.TrimSuffix(bufferedString, "\r\n")
+	switch commandString {
+	default:
+		fmt.Printf("%s: command not found", commandString)
+	}
 }
