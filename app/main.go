6ab3d8845dd9d2200967e61ae26b9456355ed782
Greg Sacciadis <greg.sacciadis@gmail.com>
2025-12-08 23:24:16 -0500

    Added the type command to check shell builtins & invalid commands (exec files check OTW)

--- app/main.go
+++ app/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"slices"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 var _ = os.Stdout
+
+var builtinCommands []string = []string{"exit", "echo", "type"}
 
 func main() {
 
@@ -29,11 +32,18 @@ func main() {
 		bufferedString := string(buffer[:numBytesRead])
 		argsSplicedFromBuffer := strings.Fields(bufferedString)
 		commandString, inputArgs := argsSplicedFromBuffer[0], argsSplicedFromBuffer[1:]
+
 		switch commandString {
 		case "exit":
 			return
 		case "echo":
 			fmt.Println(strings.Join(inputArgs, " "))
+		case "type":
+			if len(inputArgs) > 2 || (!slices.Contains(builtinCommands, inputArgs[0])) {
+				fmt.Printf("%s: not found\n", inputArgs[0])
+			} else {
+				fmt.Printf("%s is a shell builtin\n", inputArgs[0])
+			}
 		default:
 			fmt.Printf("%s: command not found\n", commandString)
 		}
